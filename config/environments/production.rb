4ae46d307b426501b19cb062cbcaa19be69ac264
Pertuvius <vandietus@gmail.com>
2020-05-08 10:08:55 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app167441490@heroku.com'],
+    :password       => ENV['t8xvuuyw4141'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
